e90d2322e77ce8f4ef59ab500b5a7fd33bbc3082
Charan Eswar <32614346+Charaneswar@users.noreply.github.com>
2019-12-07 16:37:22 +0530

    Update Reading_lookup_table.cpp

--- Reading_lookup_table.cpp
+++ Reading_lookup_table.cpp
@@ -1,29 +1,30 @@
 #include "mbed.h"
 
 LocalFileSystem local("local");
-char yaw_rate[600][25];
-char  c[25];
+char yaw_rate_temp[25];
+double yaw_rate_1[600];
+char* token;
+Serial pc(USBTX,USBRX);
 
 int main()
-{
-    FILE *fp = fopen("/local/yaw.txt", "r"); //lookup table has yaw values 
+{    pc.baud(9600);
+    FILE *fp = fopen("/local/yaw.txt", "r");
 
         for(int j=0; j<600; j++) {
     
-        fscanf(fp,"%s",c );
-           for(int i=0;i<25;i++)
-           {
-               yaw_rate[j][i]=c[i];
-
-               }
+            fscanf(fp,"%s",yaw_rate_temp );
+            token = strtok(yaw_rate_temp,",;");
+            yaw_rate_1[j] = atoi(token);
+//            yaw_rate_1[j] = yaw_rate_1[j] /100000000;
 
         }
-
+    double* a= &yaw_rate_1[0]  ;
     fclose(fp);
             for(int k=0; k<600; k++) {
-                   
-               printf(yaw_rate[k]);
-                printf("\n");
+           
+     printf("%lf \n", *a);
+          a++;
+
                }
 
 }
