323c11921f6835e7e84351ef1c4b65c998f1f060
Charan Eswar <32614346+Charaneswar@users.noreply.github.com>
2019-12-07 16:47:18 +0530

    Update Reading_lookup_table.cpp

--- Reading_lookup_table.cpp
+++ Reading_lookup_table.cpp
@@ -15,7 +15,7 @@ int main()
             fscanf(fp,"%s",yaw_rate_temp );
             token = strtok(yaw_rate_temp,",;");
             yaw_rate_1[j] = atoi(token);
-//            yaw_rate_1[j] = yaw_rate_1[j] /100000000;
+          yaw_rate_1[j] = yaw_rate_1[j] /1000000000;
 
         }
     double* a= &yaw_rate_1[0]  ;
